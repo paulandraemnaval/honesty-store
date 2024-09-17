b58ff2b1ecad4b5ade26d4f844c5b20c5639bf30
navs <22-1-00759@vsu.edu.ph>
2024-09-17 15:18:38 +0800

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="sm:hidden flex    ">MOBILE</div>
+      <div className="sm:flex hidden">DESKTOP</div>
+    </nav>
+  );
+};
+
+export default Navbar;
