33a1396c69dee44f1533147894f6fdfeb083f61d
Bea Ramirez <21-1-01483@vsu.edu.ph>
2024-10-02 15:56:52 +0800

    signing post2

--- app/api/admin/signin/route.js
+++ app/api/admin/signin/route.js
@@ -1,6 +1,15 @@
 import { auth, db } from "@utils/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { NextResponse } from "next/server";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  Timestamp,
+  updateDoc,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 
 const signInUser = async (email, password) => {
   try {
@@ -16,17 +25,30 @@ const signInUser = async (email, password) => {
   }
 };
 
-export async function GET(request) {
+export async function POST(request) {
   try {
     const reqFormData = await request.formData();
     const { email, password } = Object.fromEntries(reqFormData);
     const accountData = await signInUser(email, password);
+
     if (accountData instanceof Error) {
       console.log("Error in user sign-up:", accountData);
       return NextResponse.json({ error: accountData.message }, { status: 400 });
     }
+
+    const sessionRef = collection(db, "Session");
+    const sessionDoc = doc(sessionRef);
+
+    const sessionData = {
+      session_id: sessionDoc.id,
+      account_id: accountData.account_id,
+      session_timestamp: Timestamp.now().toDate(),
+    };
+
+    await setDoc(sessionDoc, sessionData);
+
     return NextResponse.json(
-      { message: "Account signed-up successfully", accountData },
+      { message: "Account signed-up successfully", accountData, sessionData },
       { status: 200 }
     );
   } catch (error) {
