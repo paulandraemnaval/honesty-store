b964edc30fb38ed67777e4958e7f9fc08a752d25
navs <22-1-00759@vsu.edu.ph>
2024-10-12 23:47:33 +0800

    finished product, inven, supplier, and category. log entity wip

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <nav className="flex">
       {/*desktop nav*/}
-      <div className="sm:flex hidden h-full flex-col bg-green-400 gap-6 py-8 px-2 max-w-16">
-        <div className=" border-gray-300 justify-center align-center flex mb-auto text-center">
-          <Link href="/admin/user">dashboard</Link>
+      <div className="sm:flex hidden h-full items-center justify-center flex-col bg-green-400 gap-6 py-8 px-2 max-w-20">
+        <div className=" border-gray-300 justify-center align-center text-center">
+          <Link href="/admin/user">Dash</Link>
         </div>
-        <div className=" border-gray-300 justify-center align-center flex text-center">
+        <div className=" border-gray-300 justify-center align-center text-center">
           <Link href="/admin/user/products">products</Link>
         </div>
-        <div className=" border-gray-300 justify-center align-center flex">
+        <div className=" border-gray-300 justify-center align-center text-center">
           <Link href="/admin/user/manage">Manage</Link>
         </div>
         <div className=" border-gray-300 justify-center align-center flex mt-auto">
