3cc64c658433e64191564044e50a0e184ebfc711
Bea Ramirez <21-1-01483@vsu.edu.ph>
2024-11-16 20:23:11 +0800

    update query

--- app/api/admin/query/route.js
+++ app/api/admin/query/route.js
@@ -31,11 +31,16 @@ export async function GET(request) {
       const snapshot = await getDocs(q);
       const suppliers = snapshot.docs.map((doc) => doc.data());
 
-      const filteredSuppliers = suppliers.filter((item) => {
-        return item.supplier_name
-          .toLowerCase()
-          .includes(supplier.toLowerCase());
-      });
+      const filteredSuppliers = suppliers
+        .filter((item) => {
+          return item.supplier_name
+            .toLowerCase()
+            .includes(supplier.toLowerCase());
+        })
+        .map((item) => ({
+          supplier_name: item.supplier_name,
+          supplier_id: item.supplier_id,
+        }));
 
       filteredSuppliers.sort((a, b) => {
         const aSimilarity = calculateSimilarity(a.supplier_name, supplier);
@@ -55,9 +60,16 @@ export async function GET(request) {
       const snapshot = await getDocs(q);
       const products = snapshot.docs.map((doc) => doc.data());
 
-      const filteredProducts = products.filter((item) => {
-        return item.product_name.toLowerCase().includes(product.toLowerCase());
-      });
+      const filteredProducts = products
+        .filter((item) => {
+          return item.product_name
+            .toLowerCase()
+            .includes(product.toLowerCase());
+        })
+        .map((item) => ({
+          product_name: item.product_name,
+          product_id: item.product_id,
+        }));
 
       filteredProducts.sort((a, b) => {
         const aSimilarity = calculateSimilarity(a.product_name, search);
@@ -77,11 +89,16 @@ export async function GET(request) {
       const snapshot = await getDocs(q);
       const categories = snapshot.docs.map((doc) => doc.data());
 
-      const filteredCategory = categories.filter((item) => {
-        return item.category_name
-          .toLowerCase()
-          .includes(category.toLowerCase());
-      });
+      const filteredCategory = categories
+        .filter((item) => {
+          return item.category_name
+            .toLowerCase()
+            .includes(category.toLowerCase());
+        })
+        .map((item) => ({
+          category_name: item.category_name,
+          category_id: item.category_id,
+        }));
 
       filteredCategory.sort((a, b) => {
         const aSimilarity = calculateSimilarity(a.category_name, category);
