f55ca11c336857d20900afdd4ca49bea8c4f0daa
PaulNaval <22-1-00759@vsu.edu.ph>
2024-11-20 09:20:45 +0800

    wip products inventory page

--- app/admin/user/products/edit_inventory/[product_id]/page.jsx
+++ app/admin/user/products/edit_inventory/[product_id]/page.jsx
@@ -1,7 +1,36 @@
 "use client";
+import { useEffect, useState } from "react";
 const page = ({ params }) => {
+  const [loading, setLoading] = useState();
+  const [productInventories, setProductInventories] = useState([]);
+  useEffect(() => {
+    const getProductInventories = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `/api/admin/inventory?productId=${params.product_id}`
+        );
+        const data = await response.json();
+        console.log(data?.data);
+        setProductInventories(Array.isArray(data?.data) ? data?.data : []);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getProductInventories();
+  }, []);
   console.log(params.product_id);
-  return <div>page</div>;
+  return (
+    <div>
+      {productInventories.map((productInventory) => (
+        <div key={productInventory.inventory_id}>
+          <div>{productInventory.inventory_id}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default page;
