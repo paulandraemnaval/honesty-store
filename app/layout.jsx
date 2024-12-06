97f050caa2875abe3326345442c769ec0055fedb
PaulNaval <22-1-00759@vsu.edu.ph>
2024-12-06 09:36:11 +0800

    product modal

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,18 +12,34 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/metaIcons/tab_icon.png" />
-
-        <meta property="og:title" content="Honesty Store" />
-        <meta
-          property="og:description"
-          content="The best store with the best prices!"
-        />
-        <meta property="og:image" content="/metaIcons/open_graph_icon.png" />
-        <meta property="og:url" content="https://honesty-store.vercel.app" />
-        <meta property="og:type" content="website" />
       </head>
-
       <body className="bg-backgroundMain">
+        <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          <meta property="og:title" content="Honesty Store" />
+          <meta
+            property="og:description"
+            content="The best store with the best prices!"
+          />
+          <meta
+            property="og:image"
+            content="https://honesty-store.vercel.app/metaIcons/open_graph_icon.png"
+          />
+          <meta property="og:url" content="https://honesty-store.vercel.app" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Honesty Store" />
+          <meta
+            name="twitter:description"
+            content="The best store with the best prices!"
+          />
+          <meta
+            name="twitter:image"
+            content="https://honesty-store.vercel.app/metaIcons/open_graph_icon.png"
+          />
+        </Head>
+
         <div>
           <Toaster reverseOrder={false} />
         </div>
